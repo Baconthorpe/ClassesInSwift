2bd05d710ee3b0e85ba4f2ecaca16515ba48bb67
Baconthorpe <zabuhoff@gmail.com>
2016-05-27 10:34:32 -0400

    Initial Commit

---
+++ ClassesInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassesInSwift
+//
+//  Created by Ezekiel Abuhoff on 5/27/16.
+//  Copyright © 2016 Ezekiel Abuhoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
